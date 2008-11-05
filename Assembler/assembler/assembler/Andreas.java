445b59b9b18806cddd1106421d4c467acdd954f0
arurke <arurke@3f7a566c-7f07-11dd-a5c9-f1483cedc3eb>
2008-11-05 07:15:04 +0000

    Andreas class

---
+++ Assembler/assembler/assembler/Andreas.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package assembler;
+
+/**
+ * @author Andreas
+ *
+ */
+public class Andreas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
