cc594470a120e7bf2f2ba292b401199763d11b97
arurke <arurke@3f7a566c-7f07-11dd-a5c9-f1483cedc3eb>
2008-11-06 03:12:24 +0000

    Creation

---
+++ Assembler/assembler/assembler/SymbolTable.java
@@ -0,0 +1,32 @@
+package assembler;
+
+import java.util.HashMap;
+
+/*
+* @author Andreas Urke
+* @author Magnus Lervag
+* @since   05.11.2008
+* @version 05.11.2008
+* 
+* SymbolTable contains a HashMap with the Symbols as values and their label
+* as keys, and provide a get and a insert method.
+*/
+public class SymbolTable {
+	private HashMap<String, Symbol> symTab = new HashMap<String, Symbol>();
+	
+	/**
+	 * Puts a Symbol into the symTab.
+	 * @return Null if no value was paired with the key. The value if otherwise
+	 */
+	public Symbol insertSymbol(String label, Symbol symbol){
+		return symTab.put(label, symbol);
+	}
+	
+	/**
+	 * Gets a Symbol from the symTab.
+	 * @return Null if no value was paired with the key. The value if otherwise
+	 */
+	public Symbol getSymbol(String label){
+		return symTab.get(label);
+	}
+}
