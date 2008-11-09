e656b5c1278582a23facbf1dffa005e4c3861aa4
mlervaag <mlervaag@3f7a566c-7f07-11dd-a5c9-f1483cedc3eb>
2008-11-09 02:27:55 +0000

    lagt til magnusmetoder

--- Assembler/assembler/assembler/TwoPass.java
+++ Assembler/assembler/assembler/TwoPass.java
@@ -16,6 +16,7 @@ public class TwoPass
 	private AL assemblyLine;
 	private int lineNumber = 0;
 	private BufferedWriter outOverview;
+	private BufferedWriter outRecord;
 	private AL interMediateAssemblyLine;
 	private String locctr = "000000";
 	private ArrayList<InterMediateLine> intermediateLines = new ArrayList<InterMediateLine>();
@@ -39,6 +40,7 @@ public class TwoPass
 
 		// i.e.  Y O U R  C O D E ! ! ! !
 		
+		firstPass();
 		secondPass();
 		
 		try {
@@ -51,7 +53,54 @@ public class TwoPass
 		
 		
 	}
-	
+//	PASS ONE
+	public void firstPass(){
+
+		assemblyLine = alstr.nextAL();
+		
+
+		if (assemblyLine.getOpmnemonic() == "START"){
+
+			startAddress = assemblyLine.getOperand1();
+			while(startAddress.length() < 6) startAddress = "0" + startAddress;
+
+			locctr = intToHex((Integer.parseInt(startAddress)));
+			
+			InterMediateLine currentInterMediateLine = new InterMediateLine(locctr, assemblyLine);
+			intermediateLines.add(currentInterMediateLine);
+			
+			assemblyLine = alstr.nextAL();
+		}
+		else locctr = "000000";
+		
+		
+		
+		while(!alstr.atEnd()){
+			InterMediateLine currentInterMediateLine = new InterMediateLine(locctr, assemblyLine);
+			intermediateLines.add(currentInterMediateLine);
+			
+			if(!assemblyLine.isFullComment()){
+				if(assemblyLine.getLabel() != ""){
+					if(symTab.containsKey(assemblyLine.getLabel())){
+						//						TODO: finne ut korsjen vi kasta skikkeelige exeptions
+						//						throw new IllegalDuplicateError;
+					}
+					else {
+						Symbol sym = new Symbol(locctr, "", 0, 0);
+						symTab.put(assemblyLine.getLabel(), sym);
+					}
+				}
+				if(assemblyLine.getOpmnemonic() != ""){
+					correctLOCCTR(assemblyLine);
+				}
+				//				else throw new InvalidOpcode;
+			}
+			
+			assemblyLine = alstr.nextAL();
+		}
+		String programLength = hexMath(locctr, '-' ,startAddress);
+		while(programLength.length() < 6) programLength = "0" + programLength;
+	}
 	//The Second pass
 	public void secondPass(){
 		iter = intermediateLines.listIterator();
@@ -108,7 +157,116 @@ public class TwoPass
 			printToOverviewFile(interMediateAssemblyLine);
 		}
 	}
-	
+//	Makes object code
+	public String makeObjectCode() throws IOException{
+
+		String objectCode, opCodeValue, programCounter, base, targetAddress, disp, x="0", b="0", p="0", e="0";
+
+		//		get value of mnemonic in optable
+
+		OpCode tempOpCode = new OpCode(assemblyLine.getOpmnemonic());
+		OpTable opTable = new OpTable();
+		opCodeValue = opTable.lookup_AsHex(assemblyLine.getOpmnemonic());
+
+		//		get value of targetAddress(TA)
+		targetAddress = operand1;
+
+		if (tempOpCode.getFormat() == 2){
+			opCodeValue = hexMath(opCodeValue, '+', "3");
+			while (opCodeValue.length() < 2) opCodeValue = "0" + opCodeValue;
+			if(operand1 == "") operand1 = "0";
+			if(operand2 == "") operand2 = "0";
+			objectCode = opCodeValue + operand1 + operand2;
+		}
+
+		else{
+			//		check indirect and immediate
+			if(assemblyLine.isIndirect() && !assemblyLine.isImmediate())	
+				opCodeValue = hexMath(opCodeValue, '+', "2");
+
+			else if(!assemblyLine.isIndirect() && assemblyLine.isImmediate())
+				opCodeValue = hexMath(opCodeValue, '+', "1");
+
+			else opCodeValue = hexMath(opCodeValue, '+', "3");
+
+			//		check index
+			if(assemblyLine.isIndexed())
+				x="1";
+
+			//		check if pcPossible and if true, get PC
+			if(isPCPossible()){
+				p="1";
+				b="0";
+
+				//			getting the PC from next lines locctr then resetting the locctr
+				
+				
+				
+				InterMediateLine currentInterMediateLine = iter.next();
+				interMediateAssemblyLine = currentInterMediateLine.getAssemblyLine();
+				programCounter = currentInterMediateLine.getLocctr();				
+				
+				currentInterMediateLine = iter.previous();	
+
+				//			setting disp for PC-relative(=TA-PC)
+				disp = hexMath(targetAddress, '-', programCounter);
+				while (disp.length() < 3) disp = "0" + disp;
+			}
+
+			//		check if basePossible and if true, get basevalue
+			else if (isBasePossible()){
+				p="0";
+				b="1";
+
+				//			get basevalue
+				base = this.base;
+
+				//			setting disp for base-relative(=TA-BASE)
+				disp = hexMath(targetAddress, '-', base);
+				while (disp.length() < 3) disp = "0" + disp;
+
+			}
+			else {
+				p="0";
+				b="0";
+				disp = targetAddress;
+			}
+			//		check if extended
+			if(assemblyLine.isExtended()){
+				e="1";
+				disp = targetAddress;
+				while (disp.length() < 5) disp = "0" + disp;	
+			}
+
+
+			String flagHex = x+p+b+e; 
+			int i= Integer.parseInt(flagHex,2);
+			flagHex = Integer.toHexString(i);
+
+			//		make objectcode
+			while (opCodeValue.length() < 2) opCodeValue = "0" + opCodeValue;
+
+			objectCode = opCodeValue + flagHex + disp;
+		}
+
+		return objectCode;
+	}
+
+	public String hexMath(String hex1, char operator, String hex2){
+		if (operator=='+'){
+			int i1= Integer.parseInt(hex1,16);
+			int i2= Integer.parseInt(hex2,16);
+			hex1 = Integer.toHexString(i1+i2);
+		}
+		if(operator=='-'){
+			int i1= Integer.parseInt(hex1,16);
+			int i2= Integer.parseInt(hex2,16);
+			hex1 = Integer.toHexString(i1-i2);
+		}
+		//		else throw IllegalOperatorExeption;
+		return hex1;	
+	}
+
 	//Searches for BASE and NOBASE and processes it.
 	public void searchAndProcessBase(AL assemblyLine){
 		//IF BASE
@@ -324,6 +482,46 @@ public class TwoPass
 		else LengthOfByte = (constant.length()-3);
 		return LengthOfByte;
 
+	}
+	public void printToRecord(String objectCode){
+		//		Takes an objectcode string and prints it as a new line in the RecordFile
+		try {
+			if(outRecord == null) 
+				outRecord = new BufferedWriter(new FileWriter("Record", true));
+			outRecord.write(objectCode);
+		} catch (IOException e) {
+		}
+	}
+	public Boolean isPCPossible(){
+		//		When program-counter relative mode is used, disp
+		//		is a 12-bits signed integer
+		//		2’s complement
+
+		//		TA=(PC)+ disp 
+		//		Program-counter relative b=0,p=1
+
+		if ((-2048 <= Integer.parseInt(targetAddress, 16) && Integer.parseInt(targetAddress, 16) <= 2047))
+			return true;
+		else return false;
+	}
+	public Boolean isBasePossible(){
+		//		When base relative mode is used, disp is a 12-bits
+		//		unsigned integer
+
+		//		TA=(B)+disp
+		//		Base relative b=1,p=0 
+
+		if(0 <= Integer.parseInt(targetAddress, 16) && Integer.parseInt(targetAddress, 16) <=4095 && baseFlag == true)
+			return true;
+		else return false;
+	}
+
+	public String intToHex(int inputFromUser){
+		//		Takes a decimal int from user and converts it to hex and returns string
+
+		int i = inputFromUser;
+		String s = Integer.toHexString(i);
+		return s;
 	}
 
 }
